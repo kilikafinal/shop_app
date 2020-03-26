8c8ee45997c12f8aa273ec53df7c4f0ae6547670
Javin Hung <kilikafinal@gmail.com>
2020-03-26 08:15:00 -0700

    Added image input row

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -10,13 +10,31 @@ class EditProductScreen extends StatefulWidget {
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
   final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
 
 
   @override
+  void initState() {
+_imageUrlFocusNode.addListener(_updateImageUrl);
+  }
+
+  @override
   void dispose() {
-_priceFocusNode.dispose();
-_descriptionFocusNode.dispose();
-super.dispose();
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _updateImageUrl(){
+    if( !_imageUrlFocusNode.hasFocus){
+      setState(() {
+
+      });
+    }
   }
 
   @override
@@ -31,29 +49,53 @@ super.dispose();
           child: SingleChildScrollView(
             child: Column(
               children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "Title"),
-                      textInputAction: TextInputAction.next,
-                      onFieldSubmitted: (_) {
-                        FocusScope.of(context).requestFocus(_priceFocusNode);
-                      },
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Title"),
+                  textInputAction: TextInputAction.next,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_priceFocusNode);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Price"),
+                  textInputAction: TextInputAction.next,
+                  keyboardType: TextInputType.number,
+                  focusNode: _priceFocusNode,
+                  onFieldSubmitted: (_) {
+                    FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: "Description"),
+                  maxLines: 3,
+                  keyboardType: TextInputType.multiline,
+                  focusNode: _descriptionFocusNode,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                      width: 100,
+                      height: 100,
+                      margin: EdgeInsets.only(top: 8, right: 10),
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.grey)),
+                      child: _imageUrlController.text.isEmpty
+                          ? Text("Enter URL")
+                          : Image.network(_imageUrlController.text, fit: BoxFit.cover,),
                     ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "Price"),
-                      textInputAction: TextInputAction.next,
-                      keyboardType: TextInputType.number,
-                      focusNode: _priceFocusNode,
-                      onFieldSubmitted: (_) {
-                        FocusScope.of(context).requestFocus(_descriptionFocusNode);
-                      },
-                    ),
-                    TextFormField(
-                      decoration: InputDecoration(labelText: "Description"),
-                      maxLines: 3,
-                      keyboardType: TextInputType.multiline,
-                      focusNode: _descriptionFocusNode,
-                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: "Image URL"),
+                        keyboardType: TextInputType.url,
+                        textInputAction: TextInputAction.done,
+                        controller: _imageUrlController,
+                        focusNode: _imageUrlFocusNode,
+                      ),
+                    )
                   ],
+                )
+              ],
             ),
           ),
         ),
